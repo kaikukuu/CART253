4f3c6f1b5ce6880b6c72f88c77f6230a0149223f
allecatt <157419169+allecatt@users.noreply.github.com>
2024-10-28 22:10:26 -0400

    Completed speech-speech arrays exercise

--- p5-projects/arrays/speech-speech/js/script.js
+++ p5-projects/arrays/speech-speech/js/script.js
@@ -1,25 +1,41 @@
 /**
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ *“Speech! Speech!”?
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+let speech = ["Veni.", "Vidi.", "Vici.", "Sensi malum."];
+let speechIndex = 0;
 
+function setup() {
+    createCanvas(600, 100);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    background(0);
 
+    // Get the current line of our speech
+    let currentLine = speech[speechIndex];
+
+    // Display it
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER); // Centered horizontally and vertically
+    text(currentLine, width / 2, height / 2); // Centered on the canvas
+    pop();
+}
+
+function mousePressed() {
+    // Increase the index
+    speechIndex = speechIndex + 1;
+    // Check if the index is too high for the array's length!
+    if (speechIndex >= speech.length) {
+        // If so, reset it to 0 (go back to the beginning)
+        speechIndex = 0;
+    }
+
+    // If you wanted to be very fancy you can use modulo instead:
+    // speechIndex = (speechIndex + 1) % speech.length;
 }
