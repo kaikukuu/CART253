bc7b0c8c79be358e5ec6d265c3cfa0fa6a76b9e1
allecatt <157419169+allecatt@users.noreply.github.com>
2024-10-28 23:21:10 -0400

    completed debugging arrays

--- p5-projects/arrays/debugging-arrays/js/script.js
+++ p5-projects/arrays/debugging-arrays/js/script.js
@@ -8,7 +8,7 @@
 "use strict";
 
 // The bugs array (starts empty)
-let bugs;
+let bugs = [];
 
 // Time between bugs. This will get bigger.
 const minimumBugDelay = 0.5 * 1000;
@@ -20,7 +20,6 @@ let bugDelay = maximumBugDelay;
 */
 function setup() {
     createCanvas(600, 600);
-
     setTimeout(addBug, bugDelay);
 }
 
@@ -28,14 +27,12 @@ function setup() {
  * Adds a bug to the array, updates the timer to get faster
  */
 function addBug() {
-    // Create and add a bug
     const bug = createBug();
+    bugs.push(bug); // Add the bug to the array
 
     // Reduce the delay
     bugDelay -= random(0, 100);
-    // Constrain it (so they don't come TOO fast)
     bugDelay = constrain(bugDelay, minimumBugDelay, maximumBugDelay);
-    // Set the new timeout
     setTimeout(addBug, bugDelay);
 }
 
@@ -64,7 +61,7 @@ function draw() {
     background("#ddeeff");
 
     // Move and draw the bugs
-    for (let bug of bugs {
+    for (let bug of bugs) {
         moveBug(bug);
         drawBug(bug);
     }
@@ -93,16 +90,12 @@ function drawBug(bug) {
  * Removes bugs if you click (near) them
  */
 function mousePressed() {
-    // We need to check EVERY bug to see if it was clicked
-    for (let bug of bags) {
-        // Get the distance between the mouse and the bug
+    // Check every bug to see if it was clicked
+    for (let bug of bugs) {
         const d = dist(mouseX, mouseY, bug.x, bug.y);
-        // Check if it's close enough
-        if (d < bug.size * 2) {
-            // If so get the index of this bug in the bugs array
+        if (d < bug.size / 2) {
             const index = bugs.indexOf(bug);
-            // And remove it
-            bugs.spice(index, 10);
+            bugs.splice(index, 1); // Remove one element at the index
         }
     }
 }
