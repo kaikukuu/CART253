dc8f3bef13f7fd3bf2193b0397314c2b7a8ceffc
allecatt <157419169+kaikukuu@users.noreply.github.com>
2024-11-11 21:22:58 -0500

    Completed tarot example JSON project

--- p5-projects/data/machines/js/script.js
+++ p5-projects/data/machines/js/script.js
@@ -10,37 +10,91 @@
 
 "use strict";
 
-/**
- * Create the canvas
- */
+// /**
+//  * Create the canvas
+//  */
+// function setup() {
+//     createCanvas(400, 200);
+// }
+
+// /**
+//  * Display the three machines
+//  */
+// function draw() {
+//     background(0);
+
+//     // Incinerator
+//     push();
+//     noStroke();
+//     fill("#ff4400");
+//     rect(0, 100, 100, 100);
+//     pop();
+
+//     // Freezer
+//     push();
+//     noStroke();
+//     fill("#bbbbff");
+//     rect(150, 100, 100, 100);
+//     pop();
+
+//     // Crusher
+//     push();
+//     noStroke();
+//     fill("#777777");
+//     rect(300, 100, 100, 100);
+//     pop();
+// }
+
+const machineWidth = 100;
+const machineHeight = 100;
+
+// An array of machines data
+// We don’t necessarily use type yet, but it’s clearly an important part of what a machine is and does, so it makes sense to include it. It will enable us to distinguish the different machines and have our program act differently depending on the machine in question.
+let machines = [
+    {
+        type: "incinerator",
+        x: 0,
+        y: 100,
+        width: machineWidth,
+        height: machineHeight,
+        fill: "#ff4400",
+    },
+    {
+        type: "freezer",
+        x: 150,
+        y: 100,
+        width: machineWidth,
+        height: machineHeight,
+        fill: "#bbbbff",
+    },
+    {
+        type: "crusher",
+        x: 300,
+        y: 100,
+        width: machineWidth,
+        height: machineHeight,
+        fill: "#777777"
+    }
+];
+
 function setup() {
     createCanvas(400, 200);
 }
 
-/**
- * Display the three machines
- */
 function draw() {
     background(0);
 
-    // Incinerator
-    push();
-    noStroke();
-    fill("#ff4400");
-    rect(0, 100, 100, 100);
-    pop();
+    // Go through the machine in the array and draw them
+    for (let machine of machines) {
+        drawMachine(machine);
+    }
 
-    // Freezer
-    push();
-    noStroke();
-    fill("#bbbbff");
-    rect(150, 100, 100, 100);
-    pop();
+}
 
-    // Crusher
+function drawMachine(machine) {
     push();
     noStroke();
-    fill("#777777");
-    rect(300, 100, 100, 100);
+    fill(machine.fill);
+    rect(machine.x, machine.y, machine.width, machine.height);
     pop();
 }
